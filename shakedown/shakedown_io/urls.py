88c122f8eb7c801c32b17f2050cda51df7ea21e2
Benjamin Blount <badblount@gmail.com>
2020-12-13 18:30:34 -0600

    Committing working version 1.

--- shakedown/shakedown_io/urls.py
+++ shakedown/shakedown_io/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('file/<int:id>', views.file),
+    path('upload', views.upload_page),
+    path('upload_file', views.upload)
+    # path('file/<int:id>/edit', views.edit_file),
 ]
